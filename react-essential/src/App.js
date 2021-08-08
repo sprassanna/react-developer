356db926c92e5bdd91a763eb4bb5a614d663d031
Prassanna <iamprassanna@gmail.com>
2021-08-08 20:16:49 +0530

    Updated App Component to render properties using props

--- react-essential/src/App.js
+++ react-essential/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function MAIN(props) {
+  return (
+    < header >
+      <h1>SAMPLE APP by {props.name}</h1>
+    </header >
+  )
+}
+
+function CONTENT() {
+  return (
+    < section >
+      <h2>How react works</h2>
+    </section >
+  )
+}
+
+function FOOTER(props) {
+  return (
+    <footer>
+      <h3>passion to learn new features and techologies </h3>
+      <p> Copyright {props.year}</p>
+    </footer>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MAIN name="Prassanna S" />
+      <CONTENT />
+      <FOOTER year={new Date().getFullYear()} />
+
+
     </div>
   );
 }
